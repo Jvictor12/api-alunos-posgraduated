63cf61226dea6664bbd56dfb40b2b5f339f38cce
jvictor12 <01joaovictordvp@gmail.com>
2023-11-16 12:47:10 -0300

    "Add livro functions to facade"

--- api-alunos-posgraduate/src/main/java/io/github/jvictor12/apialunosposgraduate/infraestrutura/service/Facade.java
+++ api-alunos-posgraduate/src/main/java/io/github/jvictor12/apialunosposgraduate/infraestrutura/service/Facade.java
@@ -1,8 +1,10 @@
 package io.github.jvictor12.apialunosposgraduate.infraestrutura.service;
 
 import io.github.jvictor12.apialunosposgraduate.entity.Estudante;
+import io.github.jvictor12.apialunosposgraduate.entity.Livro;
 import io.github.jvictor12.apialunosposgraduate.service.ArquivoService;
 import io.github.jvictor12.apialunosposgraduate.service.EstudanteService;
+import io.github.jvictor12.apialunosposgraduate.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,8 +19,12 @@ public class Facade {
     private EstudanteService estudanteService;
 
     @Autowired
+    private LivroService livroService;
+
+    @Autowired
     private ArquivoService arquivoService;
 
+    ////////////////////////////////////// ESTUDANTE //////////////////////////////////////////////
     public Page<Estudante> estudanteFindAll(PageRequest request) { return  estudanteService.findAll(request); };
 
     public Estudante estudanteFindById(Long id ) { return estudanteService.findById(id); };
@@ -28,4 +34,16 @@ public class Facade {
     public Estudante estudanteUpdate(Estudante estudante) { return estudanteService.update(estudante); };
 
     public void estudanteDelete (Long id) { estudanteService.delete(id); };
+
+    ////////////////////////////////////// LIVRO //////////////////////////////////////////////
+
+    public Livro livroFindById(Long id) { return livroService.findById(id); }
+
+    public List<Livro> livroFindAll() { return livroService.findAll(); }
+
+    public Livro livroSave(Livro livro) { return livroService.save(livro); }
+
+    public Livro livroUpdate(Livro livro) { return livroService.update(livro); }
+
+    public void livroDelete(Long id) { livroService.delete(id); }
 }
